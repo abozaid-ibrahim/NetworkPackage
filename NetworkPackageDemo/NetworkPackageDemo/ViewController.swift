d380e404c5c4abd805e38d14cdf206814a0cb038
abuzeid <abuzeid7@gmail.com>
2020-04-04 11:17:29 +0200

    Include the demo

---
+++ NetworkPackageDemo/NetworkPackageDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NetworkPackageDemo
+//
+//  Created by abozaid.hamed on 04.04.20.
+//  Copyright © 2020 abozaid.hamed. All rights reserved.
+//
+
+import UIKit
+import NetworkPackage
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //todo your tests here
+    }
+
+
+}
+
